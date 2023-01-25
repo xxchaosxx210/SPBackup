fc908367221b75d496fa8ac8834abd60f3eb3dd2
xxchaosxx210 <pmillar1279@googlemail.com>
2023-01-25 04:08:35 +0000

    added a log if writing token to file

--- globals/token.py
+++ globals/token.py
@@ -1,19 +1,29 @@
 import json
 import os
 import globals.config
+import logging
 
 TOKEN_PATH = os.path.join(globals.config.APP_SETTINGS_DIR, ".token.json")
+
+_Log = logging.getLogger()
+
 
 def check_data_dir_exists():
     """checks the data path exists if doesnt then dir will be created
     """
-    if not os.path.exists(globals.config.APP_DATA_DIR):
+    try:
         os.makedirs(globals.config.APP_DATA_DIR)
+    except Exception as err:
+        if isinstance(err, FileExistsError):
+            return
+        _Log.error(err.__str__())
+
 
 def save(token: str, path: str = TOKEN_PATH):
     check_data_dir_exists()
     with open(path, "w") as fp:
         fp.write(json.dumps({"token": token}))
+
 
 def load(path: str = TOKEN_PATH):
     check_data_dir_exists()
@@ -22,6 +32,7 @@ def load(path: str = TOKEN_PATH):
             return json.loads(fp.read())
     except FileNotFoundError:
         return {"token": None}
+
 
 def remove(path: str = TOKEN_PATH):
     check_data_dir_exists()
